446f6b83392fff08b5814532e811bd31b9366591
支玉栋 <yudong.zhi@easygroup.ca>
2020-07-30 15:10:22 +0800

    支持less(修复less-loader版本高的问题)/支持rem

---
+++ src/views/Loading.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="less">
+div {
+  color: red;
+  height: 100px;
+  background-color: deepskyblue;
+}
+</style>
